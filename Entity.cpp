d37e88ae7a32c3082cd67a5034a1d7da31791e96
tksthii <Nnguyen0011@conestogac.on.ca>
2026-03-26 23:53:43 -0400

    refactor Entity class with constructors, getters, setters, and health logic

--- Entity.cpp
+++ Entity.cpp
@@ -1,8 +1,58 @@
 #include "Entity.h"
 
 Entity::Entity() {
-	name = "";
-	strength = 0;
-	health = 0;
-	armorClass = 0;
+    name = "";
+    strength = 0;
+    health = 0;
+    armorClass = 0;
+}
+
+Entity::Entity(std::string name, int strength, int health, int armorClass) {
+    this->name = name;
+    this->strength = strength;
+    this->health = health;
+    this->armorClass = armorClass;
+}
+
+std::string Entity::getName() const {
+    return name;
+}
+
+int Entity::getStrength() const {
+    return strength;
+}
+
+int Entity::getHealth() const {
+    return health;
+}
+
+int Entity::getArmorClass() const {
+    return armorClass;
+}
+
+void Entity::setName(std::string name) {
+    this->name = name;
+}
+
+void Entity::setStrength(int strength) {
+    this->strength = strength;
+}
+
+void Entity::setHealth(int health) {
+    this->health = health;
+}
+
+void Entity::setArmorClass(int armorClass) {
+    this->armorClass = armorClass;
+}
+
+void Entity::takeDamage(int damage) {
+    health -= damage;
+    if (health < 0) {
+        health = 0;
+    }
+}
+
+bool Entity::isAlive() const {
+    return health > 0;
 }
--- Entity.h
+++ Entity.h
@@ -2,10 +2,26 @@
 #include <string>
 
 class Entity {
-	std::string name;
-	int strength;
-	int health;
-	int armorClass;
+protected:
+    std::string name;
+    int strength;
+    int health;
+    int armorClass;
+
 public:
-	Entity();
+    Entity();
+    Entity(std::string name, int strength, int health, int armorClass);
+
+    std::string getName() const;
+    int getStrength() const;
+    int getHealth() const;
+    int getArmorClass() const;
+
+    void setName(std::string name);
+    void setStrength(int strength);
+    void setHealth(int health);
+    void setArmorClass(int armorClass);
+
+    void takeDamage(int damage);
+    bool isAlive() const;
 };
