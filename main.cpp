0402754162b3ad084f4f8a12ea063d61892e1891
tksthii <Nnguyen0011@conestogac.on.ca>
2026-03-27 00:21:43 -0400

    test Character module in main with display output

--- main.cpp
+++ main.cpp
@@ -8,18 +8,13 @@ int main() {
     Dice d20(20);
     int num = d20.role();
     d20.animation(num);
-
     std::cout << "You got a: " << num << std::endl;
 
     std::cout << "---------------------" << std::endl;
 
     // ===== TEST CHARACTER =====
     Character player("Arin", "Warrior", 10, 30, 12);
-
-    std::cout << "Player: " << player.getName() << std::endl;
-    std::cout << "Class: " << player.getClassType() << std::endl;
-    std::cout << "HP: " << player.getHealth() << std::endl;
-    std::cout << "Attack Bonus: " << player.getAttackBonus() << std::endl;
+    player.displayCharacter();
 
     return 0;
 }
