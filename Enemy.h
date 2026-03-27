c0aae1ea053c448a945bfb73924e90747408c5bd
tksthii <Nnguyen0011@conestogac.on.ca>
2026-03-27 00:54:42 -0400

    expand Enemy header with enemy type, attack power, and display function

--- Enemy.h
+++ Enemy.h
@@ -1,8 +1,18 @@
 #pragma once
 #include "Entity.h"
+#include <string>
 
-class Enemy:public Entity {
-	int x;
+class Enemy : public Entity {
+private:
+    std::string enemyType;
+
 public:
-	Enemy();
+    Enemy();
+    Enemy(std::string name, std::string enemyType, int strength, int health, int armorClass);
+
+    std::string getEnemyType() const;
+    void setEnemyType(std::string enemyType);
+
+    int getAttackPower() const;
+    void displayEnemy() const;
 };
