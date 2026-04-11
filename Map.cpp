9dcdb22d88843764bab9ed08e7c665624089dc54
Drayvenice <s1124110064@gmail.com>
2026-04-11 02:37:50 -0400

    Connect Goblin and Orc enemies to map nodes

--- Map.cpp
+++ Map.cpp
@@ -1,5 +1,8 @@
 #include "Map.h"
 #include "Enemy.h"
+#include "Goblin.h"
+#include "Orc.h"
+#include "Dragon.h"
 #include <iostream>
 
 // this is where the map will be build (the tree structure will be setup)
@@ -11,7 +14,7 @@ void mapBuild(Tree** root) {
 	insert(&temp, "You woke up in a forest near a town with a headache. You can't remember where you are or how you got here. \n \nYou start walkinig towards the town and on they way see an elderly man being attacked by a thief. Do you defend the elderly man\nInput yes or no (y or n): ");
 	
 	// ----- FIRST CHOICE LEFT PATH ----- (ATTACK THIEF)
-	insert(&temp->left, "You decid to attack the thief", NULL /*replace NULL with thief enemy here*/);
+	insert(&temp->left, "You decid to attack the thief", new Goblin());
 
 	insert(&temp->left->left, "For helping the elderly man, he offerse you a place to sleep in his village. \nDo you accept his offer? (y or n): ");
 
@@ -30,7 +33,7 @@ void mapBuild(Tree** root) {
 
 	insert(&temp->right->left, "You accept his offer and join the Rebel group.\nThe man hands you a stone dagger and brings your to his camp to rest for the night.\n(+5 damage from dagger) (+5 health from rest).\n\nThe next day the Rebal Leader announces that he needs volentears to raid a dungeon.\nYou however do not get a choice because you are new.");
 
-	insert(&temp->right->right, "The rebal pushes you and you take 1 hp from emotion damage.\n(-1 Health)\n\nNight is approching so you decide to rest in an abandoned building outside the the village.\n(-2 health from getting a pour sleep.\n In the morning the rebal leader finds you sleeping in his childhood home and attacks you.", NULL/*Rebal Enemy Here*/);
+	insert(&temp->right->right, "The rebal pushes you and you take 1 hp from emotion damage.\n(-1 Health)\n\nNight is approching so you decide to rest in an abandoned building outside the the village.\n(-2 health from getting a pour sleep.\n In the morning the rebal leader finds you sleeping in his childhood home and attacks you.", new Orc());
 
 	insert(&temp->right->right->left, "After defeating the rebel leader you take over his group. The group tells you about a dungeons that has been found and that they think they should check it out.");
 		
