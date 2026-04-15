82cac4f96e6bb7d5f82346ac25e1d318c8fa3d08
tksthii <Nnguyen0011@conestogac.on.ca>
2026-04-14 23:07:24 -0400

    add references for testing

---
+++ UnitTests/Character.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "Entity.h"
+#include "Traits.h"
+#include <string>
+
+class Character : public Entity {
+private:
+    std::string classType;
+    Traits traits;
+
+public:
+    Character();
+    Character(std::string name, std::string classType, int strength, int health, int armorClass);
+
+    std::string getClassType() const;
+    void setClassType(std::string classType);
+
+    Traits getTraits() const;
+
+    int getAttackBonus() const;
+    int getDodgeValue() const;
+
+    bool isWarrior() const;
+    bool isMage() const;
+    bool isRogue() const;
+
+    bool isValidClassType() const;
+
+    void displayCharacter() const;
+
+    static Character createFromConsole();
+};
---
+++ UnitTests/Functions.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <thread>
+#include <chrono>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+
+void sleepSec(int time);
+void sleepMil(int time);
+
+void clear();
+
+int inclusiveRand(int min, int max);
+
+void typePrint(std::string);
